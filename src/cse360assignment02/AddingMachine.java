7aae88cf94e0afaec457c623f9dd9e48d3f61154
ninkuk <ninadk03@gmail.com>
2020-10-07 21:45:22 -0700

    Implemented the all the functions to behave as intended and updated the javadoc as required.
    Signed-off-by: ninkuk <ninadk03@gmail.com>

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -2,12 +2,14 @@ package cse360assignment02;
 
 public class AddingMachine {
     private int total;
+    private String log;
 
     /**
      * The constructor creates a new AddingMachine object and instantiates the value of total to zero
      */
     public AddingMachine() {
         total = 0;  // not needed - included for clarity
+        log = "0"; // the log must be initialized to initial value of total which is zero
     }
 
     /**
@@ -16,7 +18,7 @@ public class AddingMachine {
      * @return value of total
      */
     public int getTotal() {
-        return 0;
+        return total;
     }
 
     /**
@@ -25,6 +27,8 @@ public class AddingMachine {
      * @param value is added to the total
      */
     public void add(int value) {
+        total += value;
+        log += " + " + value;
     }
 
     /**
@@ -33,6 +37,8 @@ public class AddingMachine {
      * @param value is subtracted from total
      */
     public void subtract(int value) {
+        total -= value;
+        log += " - " + value;
     }
 
     /**
@@ -41,12 +47,14 @@ public class AddingMachine {
      * @return the log of all operations
      */
     public String toString() {
-        return "";
+        return log;
     }
 
     /**
-     * clears the adding machine
+     * clears the adding machine by setting all the values back to zero
      */
     public void clear() {
+        total = 0;
+        log = "0";
     }
 }
