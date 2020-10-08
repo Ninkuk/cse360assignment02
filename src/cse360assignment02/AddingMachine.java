6169f12315286e54251c589fa081faaee2e84099
ninkuk <ninadk03@gmail.com>
2020-10-07 21:31:01 -0700

    Created the AddingMachine class and commented the functions using javadoc style comments.

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    /**
+     * The constructor creates a new AddingMachine object and instantiates the value of total to zero
+     */
+    public AddingMachine() {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * A getter for total
+     *
+     * @return value of total
+     */
+    public int getTotal() {
+        return 0;
+    }
+
+    /**
+     * This function increments the total by the given value
+     *
+     * @param value is added to the total
+     */
+    public void add(int value) {
+    }
+
+    /**
+     * This function decrements the total by the given value
+     *
+     * @param value is subtracted from total
+     */
+    public void subtract(int value) {
+    }
+
+    /**
+     * This is a custom toString for the class and print all the operations performed
+     *
+     * @return the log of all operations
+     */
+    public String toString() {
+        return "";
+    }
+
+    /**
+     * clears the adding machine
+     */
+    public void clear() {
+    }
+}
